c73002846c01c71674ca7d22984947373694230e
shjk0531 <shinjk0531@gmail.com>
2023-04-25 15:31:38 +0900

    Portal Runner

---
+++ Dongguk-Game/Portal Runner/Assets/2DPortalSystem/Assets/Scripts/Player/InputSystem.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class InputSystem : MonoBehaviour
+{
+    [SerializeField] private string InputMove;
+    [SerializeField] private KeyCode InputWeaponChange;
+    [SerializeField] private KeyCode InputJump;
+    [SerializeField] private KeyCode[] InputShootHorizontal;
+    [SerializeField] private KeyCode[] InputShootVertical;
+
+    [HideInInspector] public float direction;
+    [HideInInspector] public bool isWeaponChange;
+    [HideInInspector] public float isJump;
+    [HideInInspector] public float shootHorizontal;
+    [HideInInspector] public float shootVertical;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    void Update()
+    {
+        direction = Input.GetAxis(InputMove);
+
+        isWeaponChange = Input.GetKeyDown(InputWeaponChange);
+
+        if (Input.GetKeyDown(InputJump))
+            isJump = 1;
+        else
+            isJump = 0;
+
+        if (Input.GetKeyDown(InputShootHorizontal[0]))
+            shootHorizontal = 1;
+        else if (Input.GetKeyDown(InputShootHorizontal[1]))
+            shootHorizontal = -1;
+        else
+            shootHorizontal = 0;
+
+        if (Input.GetKeyDown(InputShootVertical[0]))
+            shootVertical = 1;
+        else if (Input.GetKeyDown(InputShootVertical[1]))
+            shootVertical = -1;
+        else
+            shootVertical = 0;
+    }
+}
